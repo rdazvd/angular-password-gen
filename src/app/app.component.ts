c5092fb612f2d34d5406023b168bc23fa045a051
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2020-07-25 18:00:32 -0300

    Feat: add bindings for include numbers/include symbols check boxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
 
   onButtonClick() {
+    console.log('includeLetters', this.includeLetters);
+    console.log('includeNumbers', this.includeNumbers);
+    console.log('includeSymbols', this.includeSymbols);
     this.password = 'my password';
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
   }
 
   getPassword() {
